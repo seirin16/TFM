5b00ed0dae8a337983f2ecec93c022c1567834c4
Alvaro Lopez <varolopez01@gmail.com>
2024-05-25 12:10:47 +0200

    Que los puertos sean clicables

--- app/src/main/java/com/example/lvarolpezfueyo_tfm/fronted/AnalisisActivity.kt
+++ app/src/main/java/com/example/lvarolpezfueyo_tfm/fronted/AnalisisActivity.kt
@@ -1,9 +1,16 @@
 package com.example.lvarolpezfueyo_tfm.fronted
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
@@ -80,6 +87,7 @@ class AnalisisActivity : AppCompatActivity() {
         // Verifica si la dirección IP es válida
         if (!isValidIP(ip)) {
             textViewResult.text = "Dirección IP no válida"
+            openPorts.text = "Nº puertos abiertos: 0"
             return
         } else {
             progressBar.visibility = View.VISIBLE
@@ -93,7 +101,8 @@ class AnalisisActivity : AppCompatActivity() {
                 .build()
 
             val format = if (checkBox.isChecked) "false" else "true"
-            val url = "https://tfm-fkng5higzq-ew.a.run.app/scan/$ip/$format"
+            //val url = "https://tfm-fkng5higzq-ew.a.run.app/scan/$ip/$format"
+            val url = "http://192.168.0.12:3000/scan/$ip/$format"
 
 
             val request = Request.Builder()
@@ -127,12 +136,28 @@ class AnalisisActivity : AppCompatActivity() {
                             val openPortsJson = json.getJSONArray("openPorts")
                             val numOpenPorts = openPortsJson.length()
 
+                            Log.d("AnalisisActivity", "numOpenPorts: $numOpenPorts")
+
                             this@AnalisisActivity.runOnUiThread {
                                 textViewResult.text = myResponse
                                 openPorts.text = "Nº puertos abiertos: ${numOpenPorts.toString()}"
                                 progressBar.visibility = View.GONE
                                 generatePDF.isEnabled = true
                                 share.isEnabled = true
+
+                                // Obtener la lista de puertos abiertos
+                                val openPortsList = mutableListOf<Int>()
+                                for (i in 0 until numOpenPorts) {
+                                    val portJson = openPortsJson.getJSONObject(i)
+                                    val port = portJson.getInt("port")
+                                    openPortsList.add(port)
+                                }
+
+                                // Establecer los puertos clicables
+                                val spannableString = setClickablePorts(myResponse, openPortsList)
+                                textViewResult.text = spannableString
+                                textViewResult.movementMethod = LinkMovementMethod.getInstance()
+                                textViewResult.highlightColor = Color.TRANSPARENT
                             }
                         }
                     }
@@ -140,37 +165,80 @@ class AnalisisActivity : AppCompatActivity() {
             })
         }
     }
+    fun setClickablePorts(nmapOutput: String, clickablePorts: List<Int>): SpannableString {
+        val spannableString = SpannableString(nmapOutput)
+
+        val portClickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                val port = getPortFromClick(view) // Obtener el puerto seleccionado por el usuario
+                if (port != null) {
+                    // Aquí puedes agregar la lógica para mostrar la información del puerto seleccionado
+                    Toast.makeText(view.context, "Puerto seleccionado: $port", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+            }
+        }
+
+        for (clickablePort in clickablePorts) {
+            val portString = "$clickablePort/tcp"
+            val startIndex = nmapOutput.indexOf(portString)
+
+            if (startIndex != -1) {
+                val endIndex = startIndex + portString.length
+                spannableString.setSpan(portClickableSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            }
+        }
+
+        return spannableString
+    }
+
+    fun getPortFromClick(view: View): Int? {
+        val text = (view as TextView).text
+        val start = view.getOffsetForPosition(view.x, view.y)
+        val end = start + 1
+
+        val portRegex = "(\\d+/tcp)".toRegex()
+        val portMatch = portRegex.find(text.subSequence(start, end))
+
+        if (portMatch != null) {
+            val portString = portMatch.groupValues[1]
+            val port = portString.toIntOrNull()
+
+            if (port != null) {
+                return port
+            }
+        }
+
+        return null
+    }
+
+
 
     fun generatePdf() {
         try {
             val directory =
                 Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-
             val file = File(directory, "scan.pdf")
             val outputStream = FileOutputStream(file)
             val writer = PdfWriter(outputStream)
-
             // Crear un objeto PdfDocument para crear el PDF
             val pdf = com.itextpdf.kernel.pdf.PdfDocument(writer)
-
             // Crear un objeto Document para agregar contenido al PDF
             val document = Document(pdf)
-
-
             // Agregar el contenido del TextView al PDF
-
             val ip = editTextIp.text.toString()
             val numOpenPorts = openPorts.text.toString()
             val text = textViewResult.text.toString()
-
             val ipParagraph = Paragraph("Dirección IP: $ip")
             val numOpenPortsParagraph = Paragraph(numOpenPorts)
             val paragraph = Paragraph(text)
-
             document.add(ipParagraph)
             document.add(numOpenPortsParagraph)
             document.add(paragraph)
-
             document.close()
             pdf.close()
             Toast.makeText(
